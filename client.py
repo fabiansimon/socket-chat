63239c93a0f64d858fa3957ffba9b907f29acbfb
Fabian Simon <77444050+fabiansimon@users.noreply.github.com>
2025-09-15 12:51:32 +1000

    added comments

--- client.py
+++ client.py
@@ -9,9 +9,16 @@ import threading
 HOST = '127.0.0.1'
 PORT = 12346
 
+
+######################################################
+# HELPER FUNCTIONS
+######################################################
+
+# share the encryption key with the server
 def share_key(s, encr_key):
     s.sendall(f"PK {encr_key}".encode())
 
+# receive the user id from the server
 def receive_user_id(s):
     data = s.recv(1024).decode()
     display(data)
@@ -20,71 +27,81 @@ def receive_user_id(s):
     user_id = data[user_id+1:data.find("]")]
     return user_id
 
-def receive(s, encr_key = None):
-    data = s.recv(1024).decode()
-    message = data
-    
-    # If encryption key is provided, decrypt the message
-    if (encr_key is not None):
-        message = decrypt(encr_key, data)
-
-    display(message)
-    
+# display the messages nicely
 def display(data):
     print(f"\n> {data}")
     print("> ", end="", flush=True)
 
+# encrypt the message using own key
 def encrypt(key, message):
     key_bytes = key.encode()
     msg_bytes = message.encode()
+
+    # simple XOR encryption
     cipher = bytes([m ^ key_bytes[i % len(key_bytes)] for i, m in enumerate(msg_bytes)])
     return cipher.hex()
 
+# decrypt the cipher using own key
 def decrypt(key, cipher):
     key_bytes = key.encode()
     cipher = bytes.fromhex(cipher)
+
+    # simple XOR decryption
     plain = bytes([c ^ key_bytes[i % len(key_bytes)] for i, c in enumerate(cipher)])
     return plain.decode()
 
+# share the chat id with the server
 def share_chat_id(s, chat_id, key):
     ciper = encrypt(key, f"CID {chat_id}")
     s.sendall(ciper.encode())
 
+# send the message to the server in the correct format
 def send_message(s, encr_key, user_id, message):
     cipher = encrypt(encr_key, message)
     s.sendall(f"MSG {cipher}".encode())
 
+# listen for messages from the server
 def listen(s, key):
     while True:
         data = s.recv(2048)
         if not data:
             break
+
+        # decode the data
         text = data.decode().strip()
         if text.startswith("MSG "):
+            # decrypt the message
             cipher = text.split(maxsplit=1)[1]
             display(decrypt(key, cipher))
 
+        # plain server messages
         else:
-            display(text)   # plain server messages
+            display(text)
+
 
 
 ######################################################
+# MAIN CLIENT
+######################################################
 
+# create a socket and connect to the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     user_id = None
 
-    # Share the encryption key
+    # share the encryption key
     encr_key = input("Please enter the encryption key you to use: ")
     share_key(s, encr_key)
 
-    # Share the chat id
+    # share the chat id
     chat_id = input("Please enter the chat id you want to join: ")
     share_chat_id(s, chat_id, encr_key)
     user_id = receive_user_id(s)
 
+    # listen for messages from the server concurrently
     threading.Thread(target=listen, args=(s,encr_key), daemon=True).start()
 
     while True:
+        # send the message to the server
         message = input("> ")
         send_message(s, encr_key, user_id, message)
--- server.py
+++ server.py
@@ -20,49 +20,70 @@ DEBUG = True
 
 CHATS = {}
 
+######################################################
+# HELPER FUNCTIONS
+######################################################
+
+# decrypt the cipher using the user key
 def decrypt(key, cipher):
     key_bytes = key.encode()
     cipher = bytes.fromhex(cipher)
+
+    # simple XOR decryption
     plain = bytes([c ^ key_bytes[i % len(key_bytes)] for i, c in enumerate(cipher)])
     return plain.decode()
 
+# encrypt the message using the user key
 def encrypt(key, message):
     key_bytes = key.encode()
     msg_bytes = message.encode()
+
+    # simple XOR encryption
     cipher = bytes([m ^ key_bytes[i % len(key_bytes)] for i, m in enumerate(msg_bytes)])
     return cipher.hex()
 
+# receive the key from the user
 def receive_key(conn):
     key = None
     while key is None:
         data = conn.recv(1024).decode()
         key = extract_input(data, "PK")
 
+        # if the key is not valid, send an error message
         if key is None:
             conn.sendall(b"Ensure you send a valid key, please try again with following format: PK <key>")
 
     return key
 
+# receive the encrypted chat id from the user
 def receive_chat_id(conn, key):
     chat_id = None
     while chat_id is None:
         data = conn.recv(1024).decode()
         debug(f"Received data: {data}")
+
+        # decrypt the data using the user key
         data = decrypt(key, data)
         debug(f"Decrypted data: {data}")
         chat_id = extract_input(data, "CID")
-        
+
+        # if the chat id is not valid, send an error message
         if chat_id is None:
             conn.sendall(b"Ensure you send a valid chat id, please try again with following format: CID <chat_id>")
 
     return chat_id
 
+# extract the input from the data using the keyword provided
 def extract_input(data, keyword):
     try:
+        # example data: CID 123456 -> CID = ChatID, 123456 = chat_id
         data = data.split()
+
+        # if the data is not valid, return None
         if len(data) < 2:
             return None
         
+        # if the keyword is found, return the input
         if data[0] == keyword:
             return data[1]
 
@@ -70,17 +91,23 @@ def extract_input(data, keyword):
         print("Error extracting input")
         return None
 
+# generate a unique user id
 def gen_user_id():
     return str(uuid.uuid4())
 
+# extract the excluded users from the message (if provided)
 def excluded_users(message):
     start = message.find("[")
     end = message.find("]")
+
+    # none excluded
     if start == -1 or end == -1:
         return []
-        
+    
+    # return the excluded users
     return message[start+1:end].split(",")
 
+# extract the message from the data using the key and user id
 def extract_message(data, key, user_id):
     data = data.split()
     cipher = data[1:][0]
@@ -89,23 +116,30 @@ def extract_message(data, key, user_id):
 
     # If no excluded users, return the message and an empty list
     if len(excluded) == 0:
+
+        # still exclude the author so they don't receive their own message
         return message, [user_id]
 
     # If there are excluded users, return the message and the excluded users
     message = message[:message.find("[")-1]
 
     # Add the user id to the excluded users and make it unique
+    # this is to ensure the author doesn't receive their own message
     excluded.append(user_id)
     excluded = list(set(excluded))
 
     return message, excluded
     
+# receive and the message from our client script
 def receive(conn, chat_id, user_id):
     data = conn.recv(1024).decode()
     key = CHATS[chat_id][user_id]["key"]
     message, excluded = extract_message(data, key, user_id)
+
+    # generate the message to be displayed
     return f"[{chat_id}] [{user_id}]: {message}", excluded
 
+# broadcast the message to all users from same chat
 def broadcast(message, chat_id, excluded_users):
     exclude = set(excluded_users)
     for user_id, info in CHATS[chat_id].items():
@@ -115,15 +149,18 @@ def broadcast(message, chat_id, excluded_users):
         # skip if the user is in the excluded users
         if user_id in exclude: continue
 
-        # encrypt the message
+        # encrypt the message according to the user key
         cipher = encrypt(key, message)
         debug(f"Sending message to {user_id}: {cipher}")
         conn.sendall(f"MSG {cipher}".encode())
 
+# simple debug function
 def debug(output): 
     if DEBUG: 
         print(f"[DEBUG] {output}")
 
+######################################################
+# HANDLE INDIVIDUAL CLIENTS
 ######################################################
 
 def handle_client(conn, addr):
@@ -131,6 +168,7 @@ def handle_client(conn, addr):
         debug(f"Connected by {addr}")
 
         ### Initial setup for the chat
+
         # Receive the encryption key
         encr_key = receive_key(conn)
         debug(f"Encryption key received: {encr_key}")
@@ -150,9 +188,11 @@ def handle_client(conn, addr):
         # Store the encryption key and user id in CHATS 
         CHATS[chat_id][user_id] = { "key": encr_key, "conn": conn }
         online_users = CHATS[chat_id]
+
         conn.sendall(f"You successfully joined the chat, online users are {online_users.keys()}\n".encode())
         conn.sendall(f"Send your message in following format: <message> [user,ids,to,exclude (optional, separate with comma)]".encode())
 
+        # listen for messages from the client and broadcast them to all other users of the same chat
         while True:
             msg, excluded = receive(conn, chat_id, user_id)
             broadcast(msg, chat_id, excluded)
@@ -161,20 +201,25 @@ def handle_client(conn, addr):
         debug(f"Error in connection: {e}")
         traceback.print_exc()
         conn.close()
-
+        
+    # finally close the connection and delete the user from the chat
     finally: 
         conn.close()
         del CHATS[chat_id][user_id]
         
 ######################################################
+# MAIN SERVER
+######################################################
 
 print("Waiting for a connection...")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    ## Allow reuse of the prot
+    ## Allow reuse of the port
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen()
 
+    # listen for connections from clients and handle them in a new thread
     while True:
         conn, addr = s.accept()
         threading.Thread(target=handle_client, args=(conn, addr), daemon=True).start()
